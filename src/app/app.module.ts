f08a9ffca6bab7cdefb63ba9b1b4cc2aa23ec27d
TU <v.chuongtu1102@gmail.com>
2022-10-29 20:09:44 +0700

    demo1

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { ItemComponent } from './components/item/item.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    ItemComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
